7c50e50a4c4cf13be00ad94e6f1e29a48cc1665f
Oluwaseun.Olusi <oluwaseun.olusi@gmail.com>
2019-03-06 05:33:44 +0100

    Initial UI implementation

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
