6257505cf1cad9939f3e8efbec38d50b62fd76b2
oluwaseun.olusi <oluwaseun.olusi@gmail.com>
2019-03-26 07:54:10 +0100

    - Added header image - Added Logo - Added favicon - Fetch daily quote from backend service - Post feedback to backend service - Added notification bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     $('#copyright').html("&copy; Dezignspiration " + new Date().getFullYear());
+
+    loadDailyQuote();
+
     var body = $("html, body");
     body.stop().animate({ scrollTop: 0 }, "250", "swing");
     var oldScrollTop = 0;
@@ -40,4 +43,68 @@ $(document).ready(function () {
         }
         oldScrollTop = scrollTop;
     });
+
+    $('#contact-form').on('submit', function (e) {
+        e.preventDefault();
+        var message = $('#message');
+        if (!message.val() || message.val() === '') {
+            alert('Please provide a message');
+            return false;
+        }
+
+        $('#submit-form').attr('disabled', true);
+
+        var html = '';
+        $.ajax({
+            url: 'https://dezignspiration.com/api/v1/feedback',
+            method: 'POST',
+            data: $('#contact-form').serialize(),
+            dataType: 'JSON',
+            success: function (response) {
+                $('#submit-form').removeAttr('disabled');
+                message.val('');
+                renderAlert('success', "Feedback sent successfully!");
+            },
+            error: function (error) {
+                $('#submit-form').removeAttr('disabled');
+                renderAlert('error', "We could not send your feedback, please try again");
+            }
+        });
+    });
 });
+
+function loadDailyQuote () {
+    var html = '';
+    $.ajax({
+        url: 'https://dezignspiration.com/api/v1/daily/quote',
+        method: 'GET',
+        dataType: 'JSON',
+        success: function (response) {
+            var data = response.data;
+            $('#daily-quote').css({ 'background-color': data.primary_color, 'color': data.secondary_color });
+            html = new EJS({
+                url: `../partials/daily_quote.ejs`
+            })
+                .render( { data });
+            $('#render-quote').html(html);
+        },
+        error: function (error) {
+            renderAlert('error', "We could not load today's daily quote");
+        }
+    });
+}
+
+function renderAlert (type, message) {
+    var notification_bar = $('.notification-bar');
+    notification_bar.remove();
+
+    html = new EJS({
+        url: "../partials/" + type + "_alert.ejs"
+    })
+        .render( { message });
+    $('body').append(html);
+    notification_bar.removeClass('fade');
+    setTimeout(function () {
+        notification_bar.addClass('fade');
+    }, 3000);
+}
